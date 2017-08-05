e93a7ff903388c27893144ef6c3af7949c249a8a
Johannes Ippen <jo@Jos-MacBook-Pro.local>
2017-08-05 13:14:26 +0200

    Added IKEA Principle + Hash Case Study

--- _plugins/image.rb
+++ _plugins/image.rb
@@ -1,26 +1,18 @@
 class Image < Liquid::Tag
-  Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
 
   def initialize(tagName, markup, tokens)
     super
 
-    if markup =~ Syntax then
-      @img = $1
-
-#      if $2.nil? then
-#          @width = 560
-#          @height = 420
-#      else
-#          @width = $2.to_i
-#          @height = $3.to_i
-#      end
-    else
-      raise "No Image provided"
-    end
+      @img = markup
+      @img, @class = @img.split(' ').map(&:strip)
+      if @class.nil? then
+        @class = "standard"
+      end
   end
 
   def render(context)
-    "<img class=\"casestudy-image\" src=\"#{@img}\">"
+    
+    "<div class=\"#{@class}\"><img class=\"casestudy-image\" src=\"#{@img}\"></div>"
   end
 
   Liquid::Template.register_tag "image", self
